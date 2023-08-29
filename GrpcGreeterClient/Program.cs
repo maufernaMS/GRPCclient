2ff456c88e522dca70826c470117d48d668a866b
maufernaMS <70165148+maufernaMS@users.noreply.github.com>
2023-08-29 11:02:36 -0600

    client

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+var httpHandler = new HttpClientHandler();
+
+// Return `true` to allow certificates that are untrusted/invalid
+httpHandler.ServerCertificateCustomValidationCallback = 
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5075", new GrpcChannelOptions { HttpHandler = httpHandler});
+
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.Read();
